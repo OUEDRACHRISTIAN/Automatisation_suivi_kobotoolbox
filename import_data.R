3bed45ba3ab22570a8cf545d85f060e8d6c1355f
OUEDRACHRISTIAN <ouedraogocyriaque1@gmail.com>
2025-03-10 08:36:05 +0100

    #premier

--- import_data.R
+++ import_data.R
@@ -1,15 +1,3 @@
----
-title: "Import data"
-output:
-  html_document: default
-  pdf_document: 
-    latex_engine: xelatex
-  word_document: default
-date: "2025-03-10"
-author: "Christian Cyriaque"
----
-
-```{r setup, include=FALSE}
 
 
 library(robotoolbox)
@@ -21,52 +9,51 @@ kobo_setup(url = "https://kf.kobotoolbox.org",
 kobo_settings() 
 
 
-```
+
 
 
 #Énumérer vos projets
-```{r}
+
 asset_list <- kobo_asset_list()
 asset_list |>
   slice_head(n = 5) |>
   knitr::kable()
 
-```
 
 
 #Recuperer les informations pour une enquetes données avec le uid
-```{r}
+
 
 asset <- kobo_asset(Sys.getenv('MY_IUD'))
 asset
-```
+
 
 
 #Recuperer les informations pour une enquetes données avec le nom de l'enquete
-```{r}
+
 asset_ml<-asset_list |>
   filter(name == "Test1") |>
   pull(uid) |>
   kobo_asset()
 
 asset_ml
-```
+
 
 
 
 #Afficher les données
-```{r}
+
 data_ml <- kobo_data(asset_ml)
 
 data_ml |>
   select(start:uuid) |>
   slice_head(n = 5) |>
   knitr::kable()
-```
+
 
 
 # Enregistrer les données sous le format RDS
-```{r}
+
 write_rds(data_ml, "mydf.rds")
 
-```
+
